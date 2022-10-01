9568b3aa553b74751152a7eaa6b14609f1ed8ad8
K1 <searchfordeveloping@gmail.com>
2022-10-01 15:57:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+DoWork(del1);
+Console.ReadLine();
+
+static void DoWork(WorkPerformedHandler del) => del(5, WorkType.GoToMeeting);
+
+
+static void WorkPerformed1(int hurs, WorkType workType) => Console.WriteLine($"WorkPerformed1 called. Hours: {hurs}");
+
+static void WorkPerformed2(int hurs, WorkType workType) => Console.WriteLine($"WorkPerformed2 called. Hours: {hurs}");
+
+
+delegate void WorkPerformedHandler(int hours, WorkType workType);
+public enum WorkType
+{
+    GoToMeeting,
+    Footbal,
+    Coding
+}
+
---
+++ Worker.cs
@@ -0,0 +1,41 @@
+﻿namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler WorkCompleted;
+            
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i+1, workType); 
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //1st approch to call an event
+            //if (WorkPerformed != null)
+            //    WorkPerformed(hours, WorkType);
+
+            //2ed approch to call an event
+            var del = WorkPerformed as WorkPerformedHandler;
+            if(del != null) 
+                del(hours, workType);
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if (WorkCompleted( != null)
+            //    WorkCompleted((this, EventArgs.Empty);
+
+            var del = WorkCompleted as EventHandler;
+
+            if (del != null) 
+                del(this, EventArgs.Empty);
+        }
+    }
+}
