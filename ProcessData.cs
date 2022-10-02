f0c649267000e2fc99b1b2283732da6dcb707394
K1 <searchfordeveloping@gmail.com>
2022-10-01 23:44:36 -0700

    using lambdas with delegates

---
+++ ProcessData.cs
@@ -0,0 +1,11 @@
+﻿namespace DelegatesAndEvents
+{
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRuleDelegate bizRule)
+        {
+            var result = bizRule.Invoke(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,33 @@
 ﻿using DelegatesAndEvents;
+using System;
 
+//using lambdas with delegates
+BizRuleDelegate addDel = (x, y) => x + y;
+BizRuleDelegate multiplyDel = (x, y) => x * y;
+
+var data = new ProcessData();
+data.Process(3, 5, addDel);
+
+
+//using anonymous methofs
 var work = new Worker();
-work.WorkPerformed += Work_WorkPerformed;
-work.WorkCompleted += Work_WorkCompleted;
+work.WorkPerformed += (s, e) => Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");  //Work_WorkPerformed;
+work.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");  //Work_WorkCompleted;
 
 work.DoWork(8, WorkType.Coding);
 
 
-void Work_WorkPerformed(object? sender, WorkPerformedEventArgs e)
-{
-    Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
-}
+//void Work_WorkPerformed(object? sender, WorkPerformedEventArgs e)
+//{
+//    Console.WriteLine($"Hours worked: {e.Hours} {e.WorkType}");
+//}
 
-void Work_WorkCompleted(object? sender, EventArgs e)
-{
-    Console.WriteLine("Worker is done");
-}
+//void Work_WorkCompleted(object? sender, EventArgs e)
+//{
+//    Console.WriteLine("Worker is done");
+//}
 
+public delegate int BizRuleDelegate(int i, int j);
 public enum WorkType
 {
     GoToMeeting,
